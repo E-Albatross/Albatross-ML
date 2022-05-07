58c7da44b874f3f1e4f5026e21811b325841590b
JuEun Ko <kojueun330@gmail.com>
2022-05-07 16:12:10 +0900

    first commit

---
+++ craft/carftMain.py
@@ -0,0 +1,85 @@
+import torch
+import torch.backends.cudnn as cudnn
+
+import cv2
+
+import craft_utils
+from model import CRAFT
+
+## parameters
+craft_model = 'craft_mlt_25k.pth' # pretrained model
+text_threshold = 0.7 # text confidence threshold
+low_text = 0.4
+link_threshold = 0.5
+cuda = False # use cuda for inference
+mag_ratio = 1.5
+border = 10 # 줄 노트 제거 -- 음절 이미지에서 위로 삭제할 크기
+
+font_images = [] # 폰트 글씨체의 줄 단위 이미지
+user_images = [] # 사용자 글씨의 줄 단위 이미지
+syllables = [] # 각 줄에 대한 음절들
+
+## 이미지 불러오기
+img_path = 'test.png'
+
+original_img = cv2.imread(img_path)
+original_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2RGB)
+canvas_size = original_img.shape[1]
+
+## 줄 별로 텍스트 자르기
+height = original_img.shape[0] // 12
+for i in range(0,10,3):
+    y = height * i
+    fnt = original_img[height*i:height*(i+1),:].copy()
+    usr = original_img[height*(i+2):height*(i+3), :].copy()
+
+    font_images.append(fnt)
+    user_images.append(usr)
+
+## 음절 단위 추출
+# load net - CRAFT
+net = CRAFT()     # initialize
+print('Loading weights from checkpoint (' + craft_model + ')')
+if cuda:
+    net.load_state_dict(craft_utils.copyStateDict(torch.load(craft_model)))
+else:
+    net.load_state_dict(craft_utils.copyStateDict(torch.load(craft_model, map_location='cpu')))
+
+if cuda:
+    net = net.cuda()
+    net = torch.nn.DataParallel(net)
+    cudnn.benchmark = False
+
+net.eval()
+
+# load data
+image_list = user_images
+for k, image in enumerate(image_list):
+    bbox= craft_utils.test_net(net, image, canvas_size, text_threshold,low_text, mag_ratio,cuda)
+
+    img_ = image.copy()
+    cropped_img = []
+    color = [(0,255,0), (0,0,255)] # bbox 확인용
+    for i in range(len(bbox)-1):
+        x, y, w, h = bbox[i]
+        x_next = bbox[i+1][0]
+        if (x+w) > x_next: err = (x+w - x_next) // 2
+        w -= err
+        cv2.rectangle(img_, (x,y), (x+w, y+h), color[i%2], 2) # bbox 확인용
+        crop = image[border:, x:x+w].copy()
+        cropped_img.append(crop)
+
+    # 마지막 인덱스
+    x, y, w, h = bbox[-1]
+    cv2.rectangle(img_, (x, y), (x+w, y + h), color[i % 2], 2)
+    crop = image[border:, x:x+w].copy()
+    cropped_img.append(crop)
+
+    syllables.append(cropped_img)
+    cv2.imshow('img',img_)
+    cv2.waitKey(0)
+
+    # 이미지 확인용
+    for img in cropped_img:
+        cv2.imshow('img',img)
+        cv2.waitKey(0)
---
+++ craft/craft_utils.py
@@ -0,0 +1,105 @@
+import numpy as np
+import cv2
+import math
+
+import torch
+from torch.autograd import Variable
+
+import imgproc
+
+from collections import OrderedDict
+
+## 모델 불러오기
+def copyStateDict(state_dict):
+    if list(state_dict.keys())[0].startswith("module"):
+        start_idx = 1
+    else:
+        start_idx = 0
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        name = ".".join(k.split(".")[start_idx:])
+        new_state_dict[name] = v
+    return new_state_dict
+
+
+def getDetBoxes(textmap, text_threshold, low_text):
+    # prepare data
+    textmap = textmap.copy()
+    img_h, img_w = textmap.shape
+
+    """ labeling method """
+    ret, text_score = cv2.threshold(textmap, low_text, 1, 0)
+    nLabels, labels, stats, centroids = cv2.connectedComponentsWithStats(text_score.astype(np.uint8), connectivity=4)
+    det = []
+
+    for k in range(1,nLabels):
+        # size filtering
+        size = stats[k, cv2.CC_STAT_AREA]
+        if size < 10: continue
+
+        # thresholding
+        if np.max(textmap[labels==k]) < text_threshold: continue
+
+        # make segmentation map
+        segmap = np.zeros(textmap.shape, dtype=np.uint8)
+        segmap[labels==k] = 255
+        x, y = stats[k, cv2.CC_STAT_LEFT], stats[k, cv2.CC_STAT_TOP]
+        w, h = stats[k, cv2.CC_STAT_WIDTH], stats[k, cv2.CC_STAT_HEIGHT]
+        niter = int(math.sqrt(size * min(w, h) / (w * h)) * 2)
+        sx, ex, sy, ey = x - niter, x + w + niter + 1, y - niter, y + h + niter + 1
+
+        # boundary check
+        if sx < 0 : sx = 0
+        if sy < 0 : sy = 0
+        if ex >= img_w: ex = img_w
+        if ey >= img_h: ey = img_h
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(1 + niter, 1 + niter))
+        segmap[sy:ey, sx:ex] = cv2.dilate(segmap[sy:ey, sx:ex], kernel)
+
+        # make box
+        np_contours = np.roll(np.array(np.where(segmap!=0)),1,axis=0).transpose().reshape(-1,2)
+        x, y, w, h = cv2.boundingRect(np_contours)
+        box = [x, y, w, h]
+
+        det.append(box)
+    det.sort(key=lambda x: x[0])
+
+    return det
+
+def adjustResultCoordinates(boxes, ratio_w, ratio_h, ratio_net = 2):
+    boxes = np.array(boxes, dtype=np.float32)
+
+    for box in boxes:
+        box[0] *= ratio_w * ratio_net
+        box[1] *= ratio_h * ratio_net
+        box[2] *= ratio_w * ratio_net
+        box[3] *= ratio_h * ratio_net
+
+    return boxes
+
+
+def test_net(net, image, canvas_size, text_threshold, low_text, mag_ratio, cuda):
+    # resize
+    img_resized, target_ratio, size_heatmap = imgproc.resize_aspect_ratio(image, canvas_size, interpolation=cv2.INTER_LINEAR, mag_ratio=mag_ratio)
+    ratio_h = ratio_w = 1 / target_ratio
+
+    # preprocessing
+    x = imgproc.normalizeMeanVariance(img_resized)
+    x = torch.from_numpy(x).permute(2, 0, 1)    # [h, w, c] to [c, h, w]
+    x = Variable(x.unsqueeze(0))                # [c, h, w] to [b, c, h, w]
+    if cuda:
+        x = x.cuda()
+
+    # forward pass
+    with torch.no_grad():
+        y, feature = net(x)
+
+    # make score and link map
+    score_text = y[0,:,:,0].cpu().data.numpy()
+    score_link = y[0,:,:,1].cpu().data.numpy()
+
+    boxes= getDetBoxes(score_text, text_threshold, low_text)
+
+    boxes= adjustResultCoordinates(boxes, ratio_w, ratio_h).astype(np.int32) # 비율 변경
+
+    return boxes
