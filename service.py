52c3421e56072d930f91d97216a41e821152533a
JuEun Ko <kojueun330@gmail.com>
2022-05-10 01:58:19 +0900

    api - craft 수정

--- service.py
+++ service.py
@@ -1,11 +1,12 @@
 import logging
+import time
 
 import bentoml
 
 import numpy as np
 import torch
 
-from bentoml.io import Image, NumpyNdarray
+from bentoml.io import Image, JSON
 from PIL.Image import Image as PILImage
 
 import utils
@@ -24,6 +25,7 @@ stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
 
+
 #
 craft_runner = bentoml.pytorch.load_runner(
     "craft:latest",
@@ -36,22 +38,25 @@ svc = bentoml.Service(
     ],
 )
 
-@svc.api(input=Image(), output=NumpyNdarray(dtype="int64"))
+@svc.api(input=Image(), output=JSON())
 async def predict(f: PILImage) -> "np.ndarray[t.Any, np.dtype[t.Any]]":
     assert isinstance(f, PILImage)
     # preprocessing
-    image = f.convert('RGB')
-    arr = np.array(image)
-    image = utils.imgproc(arr)      # resize image and nomalization
+    image = np.array(f.convert('RGB'))
+    height = image.shape[0] // 12
+    images=[]   # 줄 단위 이미지
+    for i in range(0, 10, 3):
+        usr = image[height * (i + 2):height * (i + 3), :].copy()
+        images.append(usr)
+    # load data
+    boxes = {}
+    for k, img in enumerate(images):
+        image = utils.imgproc(img)      # resize image and nomalization
+        x = torch.from_numpy(image).permute(2, 0, 1)    # [h, w, c] to [c, h, w]
 
-    x = torch.from_numpy(image).permute(2, 0, 1)    # [h, w, c] to [c, h, w]
-
-    logger.info(f'x shape = {x.shape}')     # print input shape -- for test
-
-    y = await craft_runner.async_run(x)
-
-    score_text = y[0,:,:,0].cpu().data.numpy()
-
-    boxes = utils.getDetBoxes(score_text)
+        y = await craft_runner.async_run(x)
+        score_text = y[0,:,:,0].cpu().data.numpy()
+        bbox = utils.getDetBoxes(score_text)
+        boxes['line'+str(k)] = bbox
 
     return boxes
