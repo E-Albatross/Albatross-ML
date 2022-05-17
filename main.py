8bc74a3d3e2bfff64419cbe14572ab7879f457bb
JuEun Ko <kojueun330@gmail.com>
2022-05-17 20:58:06 +0900

    deploy to heroku

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
 import argparse
 
 import bentoml
-import torch
 
-from process import CraftMain
+from process import *
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--cuda", default=False, help='enable CUDA')
-    parser.add_argument("--detection_model", default="./model/craft_mlt_25k.pth")
+    parser.add_argument("--craft_model", default="./model/craft_mlt_25k.pth", help='text detection model path')
+    parser.add_argument("--fpn_model", default="./model/fpn_last.pth", help='segmentation model path')
+    parser.add_argument("--seg_model", default="./model/seg_model.pth", help='segmentation model path')
+
+
     args = parser.parse_args()
 
     cuda = args.cuda and torch.cuda.is_available()
 
     craft = CraftMain()
+    fpn = FPNMain()
+    seg = SegmentationMain()
+
     # load model
-    model = craft.load_model(args.detection_model, cuda)
+    craft_model = craft.load_model(args.craft_model, cuda)
+    fpn_model = fpn.load_model(args.fpn_model, cuda)
+    segmentation_model = seg.load_model(args.seg_model, cuda)
 
     # save model to BentoML's standard format in a local model store
-    tag = bentoml.pytorch.save(
+    bentoml.pytorch.save(
         "craft",
-        model
+        craft_model
+    )
+    bentoml.pytorch.save(
+        "fpn",
+        fpn_model
+    )
+    bentoml.pytorch.save(
+        "segmentation",
+        segmentation_model
     )
 
 
--- service.py
+++ service.py
@@ -1,62 +1,165 @@
-import logging
-import time
+# import logging
 
 import bentoml
 
 import numpy as np
 import torch
+from torchvision import transforms
+import cv2
+# import os
 
-from bentoml.io import Image, JSON
+from bentoml.io import Image, JSON, Text
 from PIL.Image import Image as PILImage
 
 import utils
 
-# 로그 생성
-logger = logging.getLogger()
-
-# 로그의 출력 기준 설정
-logger.setLevel(logging.INFO)
-
-# log 출력 형식
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-# log 출력
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(formatter)
-logger.addHandler(stream_handler)
-
-
 #
+img_size = (224, 224)
+
+# Load the runner
 craft_runner = bentoml.pytorch.load_runner(
     "craft:latest",
 )
+# fpn_runner = bentoml.pytorch.load_runner(
+#     "fpn:latest"
+# )
+seg_runner = bentoml.pytorch.load_runner(
+    "segmentation:latest"
+)
 
 svc = bentoml.Service(
-    name="craft",
+    name="brgs_ai",
     runners=[
-        craft_runner,
+        craft_runner,  seg_runner
     ],
 )
+
+
+
+trans = transforms.Compose([
+  transforms.ToTensor(),
+  transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # imagenet
+])
+# root_dir = './output/fntout'
+# out_dir = os.path.join(root_dir, 'test')
+# cout_dir = os.path.join(out_dir, 'good_2Img')
+#
+# if not os.path.exists(out_dir):
+#     os.mkdir(out_dir)
+# if not os.path.exists(cout_dir):
+#     os.mkdir(cout_dir)
+
+boundingboxes = {}
+
 
 @svc.api(input=Image(), output=JSON())
 async def predict(f: PILImage) -> "np.ndarray[t.Any, np.dtype[t.Any]]":
     assert isinstance(f, PILImage)
+    file={}
     # preprocessing
     image = np.array(f.convert('RGB'))
     height = image.shape[0] // 12
+    border = 7
     images=[]   # 줄 단위 이미지
     for i in range(0, 10, 3):
-        usr = image[height * (i + 2):height * (i + 3), :].copy()
+        usr = image[height * (i) + border:height * (i + 1) - border, :].copy()
         images.append(usr)
-    # load data
-    boxes = {}
+
+    # 음절 분리
+    syllable_boxes = {}
+    character_boxes = {}
+    num=1
     for k, img in enumerate(images):
+        img_ = img.copy()       # bbox 확인용
         image = utils.imgproc(img)      # resize image and nomalization
         x = torch.from_numpy(image).permute(2, 0, 1)    # [h, w, c] to [c, h, w]
-
         y = await craft_runner.async_run(x)
         score_text = y[0,:,:,0].cpu().data.numpy()
-        bbox = utils.getDetBoxes(score_text)
-        boxes['line'+str(k)] = bbox
+        det = utils.getDetBoxes(score_text)
 
-    return boxes
+        cropped_img = []
+        bbox = []
+        color = [(0, 255, 0), (0, 0, 255)]  # bbox 확인용
+        for i in range(len(det)-1):
+            x, y, w, h, cy = det[i]
+            x_next = det[i + 1][0]
+            if (x + w) > x_next: w -= (x + w - x_next) // 2
+            det[i][2] = w
+            #
+            size = max(w,height-2*border)
+            crop, b = utils.crop_img(img, size, width=w, height=height-2*border, x=x)
+            bbox.append(b)
+            cropped_img.append(crop)
+        x, y, w, h, cy = det[-1]
+        size = max(w, height-2*border)
+        crop, b = utils.crop_img(img, size, width=w, height=height-2*border, x=x)
+        cropped_img.append(crop)
+        bbox.append(b)
+
+        # draw rectangle
+        for i, box in enumerate(bbox):
+            x,y,w,h,cy = box
+            cv2.rectangle(img_, (x, y), (x + w, y + h), color[i%2], 2)  # bbox 확인용
+
+        syllables_img = np.array(cropped_img)
+
+        syllable_boxes[k] = bbox
+
+        # 확인용
+        # cv2.imwrite(os.path.join(out_dir, f'good2_{k}.png'), img_)
+        #
+        # 음소 분리
+        syllables = np.where(syllables_img < 170, 1, 0).astype(np.float32) # threshold = 200
+        character_in_line = []
+        for idx, syllable in enumerate(syllables):
+            img_ = syllables_img[idx]
+            # print(f'syllable: {num}')
+            x = trans(syllable)
+            # y = await fpn_runner.async_run(x)
+            y = await seg_runner.async_run(x)
+            y = torch.sigmoid(y)
+            seg = y.data.numpy()
+            seg_result = utils.masks_to_colorimg(seg) # 확인용
+            bbox = utils.getDetBoxes_from_seg(syllable, seg)
+            colors = [(255,0,0),(0,0,255),(0,255,0)]
+            character_in_line.append(bbox)
+            # 보정 후 - concat
+            syllable_ = syllable.copy()*255
+            for i, box in enumerate(bbox):
+                x, y, w, h= box
+                cv2.rectangle(syllable_, (x, y), (x + w, y + h), colors[i], 2)  # bbox 확인용
+
+            # # 보정 후 - not concat
+            # for i, bboxes in enumerate(bbox):
+            #     for box in bboxes:
+            #         if box == -1:
+            #             continue
+            #         x, y, w, h= box
+            #         cv2.rectangle(seg_result, (x, y), (x + w, y + h), colors[i], 2)  # bbox 확인용
+
+            # # 보정 전
+            # for i, ch in enumerate(bbox):
+            #     for box in ch:
+            #         print(f'box {box}')
+            #         if not box['label']==-1:
+            #             x, y, w, h = box['box']
+            #             cv2.rectangle(seg_result, (x, y), (x + w, y + h), colors[i], 2)  # bbox 확인용
+            #
+            # cv2.imwrite(os.path.join(cout_dir,f'{str(num)}.png'),syllable_)
+            num+=1
+        character_boxes[k] = character_in_line
+
+    file['syllable']=syllable_boxes
+    file['character']=character_boxes
+    boundingboxes['1'] = file
+
+    return file
+
+@svc.api(input=Text(), output=JSON())
+async def getBbox(key):
+    if key in boundingboxes:
+        bbox = boundingboxes.pop(key)
+        # 딕셔너리 삭제하기
+    else:
+        bbox = None
+    return bbox
